f58b93e1e7417f83be47b201dc6198a974d0782d
Callum Turnbull <callumturnbull1995@gmail.com>
2025-09-25 09:20:42 +0000

    Updated the hellohttps.ts file as part of recording.

--- src/functions/HelloHttp.ts
+++ src/functions/HelloHttp.ts
@@ -3,7 +3,7 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function HelloHttp(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const name = request.query.get('name') || await request.text() || 'from week 1 Lab! :D';
 
     return { body: `Hello, ${name}!` };
 };
